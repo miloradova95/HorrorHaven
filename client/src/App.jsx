755715344e65272c6a26a9bd1800c0f6fc7f07fe
amiloradovic95 <amiloradovic95@gmail.com>
2024-06-15 11:06:35 +0200

    delete sensitive data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import MovieList from './components/MovieList';
+import Users from './components/Users';
 
 function App() {
   return (
@@ -11,12 +12,14 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/movies">Movies</Link></li>
+            <li><Link to="/users">Users</Link></li>
           </ul>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<MovieList />} />
+          <Route path="/users" element={<Users />} />
         </Routes>
       </div>
     </Router>
